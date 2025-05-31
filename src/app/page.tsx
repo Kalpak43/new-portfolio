0b0d3cfe08d3f6d74ba5497339d94fb244585289
Kalpak43 <kalpakgoshikwar123@gmail.com>
2025-05-31 07:51:14 +0000

    ADD: container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Background from "@/components/ui/background";
 import Button from "@/components/ui/button";
+import Container from "@/components/ui/container";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { FaLinkedin } from "react-icons/fa6";
@@ -10,20 +11,35 @@ export default function Page() {
     <div className="min-h-screen relative bg-transparent py-10 px-8 md:px-20 space-y-8">
       <Background />
       <div className="grid grid-cols-1 md:grid-cols-12 gap-x-8 gap-y-8">
-        <div className="min-h-[400px] w-full p-4 md:col-span-5 rounded-2xl bg-white card-gradient-background space-x-4">
+        <Container className="min-h-[400px] md:col-span-5 pr-12 space-y-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            in fuga porro aperiam corrupti! Fuga sunt asperiores soluta vero
+            alias in unde culpa recusandae aperiam aliquam. Quidem laudantium
+            facilis facere!
+          </p>
           <Button>
             <FaLinkedin /> Linkedin
           </Button>
           <Button size="icon">
             <FaLinkedin />
           </Button>
-        </div>
-        <div className="min-h-[400px] w-full p-4 md:col-span-4 rounded-2xl bg-white card-gradient-background"></div>
-        <div className="h-full w-full p-4 md:col-span-3 rounded-2xl bg-white card-gradient-background"></div>
+        </Container>
+        <Container className="md:col-span-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          excepturi eum facere voluptatem ut et ullam tenetur quibusdam nisi. Ab
+          ipsa minus ullam totam cum aliquam id, ducimus magnam aut?
+        </Container>
+        <Container className="md:col-span-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          doloremque placeat sequi autem neque, iste quibusdam nesciunt enim
+          libero unde eos quidem provident repudiandae. Tempore inventore quos
+          unde et corrupti.
+        </Container>
       </div>
       <div className="grid grid-cols-12 gap-x-8 gap-y-8">
-        <div className="min-h-[200px] w-full p-4 col-span-8 rounded-2xl bg-white card-gradient-background"></div>
-        <div className="min-h-[200px] w-full p-4 col-span-4 rounded-2xl bg-white card-gradient-background"></div>
+        {/* <div className="min-h-[200px] w-full p-4 col-span-8 rounded-2xl bg-white card-gradient-background"></div>
+        <div className="min-h-[200px] w-full p-4 col-span-4 rounded-2xl bg-white card-gradient-background"></div> */}
       </div>
     </div>
   );
---
+++ src/components/ui/container.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface CardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function Container({ children, className }: CardProps) {
+  return (
+    <div
+      className={cn(
+        "w-full py-6 px-8 rounded-2xl space-x-4 bg-radial-[at_0_0] from-[#4a526640] from-0% to-[#ffffff0a] backdrop-blur-30 border border-white/10 shadow-container",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Container;
