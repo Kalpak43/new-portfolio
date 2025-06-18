abd82594bbb27f3d4a1c7771cbb818d9fa6bab3c
Kalpak43 <kalpakgoshikwar123@gmail.com>
2025-06-18 16:56:40 +0000

    FIX: margin issue

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -29,7 +29,7 @@ function Container({ children, className }: CardProps) {
       whileInView="visible"
       viewport={{ once: true, amount: 0.1 }}
       className={cn(
-        "w-full py-6 px-4 md:px-8 rounded-2xl space-x-4 bg-radial-[at_0_0] from-[#4a526640] from-0% to-[#ffffff0a] backdrop-blur-30 border border-white/10 shadow-container",
+        "w-full py-6 px-4 md:px-8 rounded-2xl bg-radial-[at_0_0] from-[#4a526640] from-0% to-[#ffffff0a] backdrop-blur-30 border border-white/10 shadow-container",
         className
       )}
     >
